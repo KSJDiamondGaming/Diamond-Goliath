59d09a94936bffa29f88fc85c2a46727b36b9364
KSJ DIGITAL <kylemorgan@hotmail.com>
2026-05-19 23:31:45 +0100

    dev sync

--- src/dashboard/JS/pages/Security.jsx
+++ src/dashboard/JS/pages/Security.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';om 'react';
 
 export default function Security() {
   const [loading, setLoading] = useState(true);
