03070214b87d18d6edbebc6fe48e42b6d920deb2
KSJ DIGITAL <kylemorgan@hotmail.com>
2026-06-28 00:40:53 +0100

    Add unified scripts CLI system

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log('Goliath scripts CLI running');
+
+const mode = process.env.BOT_MODE || 'dev';
+
+console.log('Mode:', mode);
+
+// placeholder structure (we will expand safely next step)
