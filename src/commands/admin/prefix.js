2e7296be05857d9831cd66b83aa439de430cf616
KSJ Hub <sycomorgan@gmail.com>
2026-07-07 20:34:35 +0100

    fix prefix command core import

--- src/commands/admin/prefix.js
+++ src/commands/admin/prefix.js
@@ -11,7 +11,7 @@ const {
   setGuildPrefix,
   resetGuildPrefix,
   normalizePrefix,
-} = require('../../modules/prefix/prefixStore');
+} = require('../../core/prefix/prefixStore');
 
 const { enforceCommandAccess } = require('../../core/ui/commandAccess');
 
