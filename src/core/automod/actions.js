7a6775816f7a3a72e65e584738a7e0611d587ca2
KSJ DIGITAL <kylemorgan@hotmail.com>
2026-07-09 01:53:56 +0100

    fix(imports): repair automod security guard paths

--- src/core/automod/actions.js
+++ src/core/automod/actions.js
@@ -1,4 +1,4 @@
-const { shouldBlockOwnerDestructiveAction } = require('../../../core/security/testModeGuard');
+const { shouldBlockOwnerDestructiveAction } = require('../security/testModeGuard');
 
 const VALID_PUNISHMENTS = ['dm', 'delete', 'warn', 'timeout', 'kick', 'ban'];
 
