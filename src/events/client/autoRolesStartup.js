aebb54ca32d1ccc8c7946d4d005d8381be5dee97
KSJ Hub <sycomorgan@gmail.com>
2026-07-10 07:43:29 +0100

    feat(auto-roles): run startup recovery on client ready

---
+++ src/events/client/autoRolesStartup.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const { Events } = require('discord.js');
+const { startupAutoRoles } = require('../../modules/autoRoles/autoRoleStartup');
+
+module.exports = {
+  name: Events.ClientReady,
+  async execute(client) {
+    await startupAutoRoles(client);
+  },
+};
