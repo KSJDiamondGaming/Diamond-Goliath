ca648a06e33573299436b2ec5ad4b345dd37ee6f
KSJ Hub <sycomorgan@gmail.com>
2026-07-13 00:13:53 +0100

    refactor(scripts): remove redundant check wrapper

--- scripts/run-goliath-check.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const command = process.argv[2] || 'check';
-
-process.argv[2] = command;
-require('./goliath');
-
-const exitCode = Number.isInteger(process.exitCode) ? process.exitCode : 0;
-process.exit(exitCode);
