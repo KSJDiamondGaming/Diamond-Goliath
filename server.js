a02855b9a123922f7e2dd594197174fd5a09fa3b
KSJ Hub <sycomorgan@gmail.com>
2026-07-14 13:04:22 +0100

    refactor verification startup to flat module

--- server.js
+++ server.js
@@ -252,7 +252,7 @@ client.once('clientReady', async () => {
     runStartupTask('Tickets', () => require('./src/modules/tickets/ticketStartup').startupTickets(client)),
     runStartupTask('Roles', () => require('./src/modules/roles/rolesStartup').initializeRoles(client)),
     runStartupTask('Translation', () => require('./src/modules/translation/translationStartup').startupTranslation(client)),
-    runStartupTask('Verification', () => require('./src/modules/verification/verificationStartup').startupVerification(client)),
+    runStartupTask('Verification', () => require('./src/modules/verification').startupVerification(client)),
     runStartupTask('Goodbye', () => require('./src/modules/goodbye').startupGoodbye(client)),
     runStartupTask('Giveaways', () => require('./src/modules/giveaways/giveawayScheduler').start(client)),
   ]);
