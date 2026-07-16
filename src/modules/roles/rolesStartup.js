0c889ada5a91f29ade9fa684ac0070d5678759bd
KSJ Hub <sycomorgan@gmail.com>
2026-07-16 01:41:00 +0100

    refactor(roles): stop legacy timed role scheduler

--- src/modules/roles/rolesStartup.js
+++ src/modules/roles/rolesStartup.js
@@ -1,11 +1,24 @@
 'use strict';
 
-const { startupRoles } = require('./roleStartup');
+const roleStore = require('./roleStore');
 
 async function initializeRoles(client) {
-  return startupRoles(client);
+  const guilds = [...(client.guilds?.cache?.values?.() || [])];
+  let enabledGuilds = 0;
+  let legacyPanels = 0;
+
+  for (const guild of guilds) {
+    const section = roleStore.getRolesSection(guild.id);
+    if (section.enabled === false) continue;
+    enabledGuilds += 1;
+    legacyPanels += Object.keys(section.reactionPanels || {}).length;
+  }
+
+  return {
+    enabledGuilds,
+    legacyPanels,
+    timedRolesMovedToCanonicalModule: true,
+  };
 }
 
-module.exports = {
-  initializeRoles,
-};
+module.exports = { initializeRoles };
