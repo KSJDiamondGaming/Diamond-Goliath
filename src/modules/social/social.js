433306c48844142dfa6b095c84bde0e92fc42135
KSJ Hub <sycomorgan@gmail.com>
2026-07-16 17:55:59 +0100

    Wire generic Social delivery service

--- src/modules/social/social.js
+++ src/modules/social/social.js
@@ -8,23 +8,25 @@ const socialHistory = require('./socialHistory');
 const socialCreators = require('./socialCreators');
 const socialSimulator = require('./socialSimulator');
 const socialDiagnostics = require('./socialDiagnostics');
+const socialDelivery = require('./socialDelivery');
 const providerRegistry = require('./providerRegistry');
+
+socialManager.deliverQueuedAlert = (guildId, account, providerResult, client, meta = {}) => socialDelivery.deliver(guildId, account, providerResult, client, {
+  ...meta,
+  bypassQueue: true,
+  bypassDuplicate: true,
+  action: meta.action || 'social_queue_delivery',
+});
 
 const STARTUP_KEY = Symbol.for('goliath.social.startup');
 
 async function startup(client, options = {}) {
   if (!client?.guilds?.cache) throw new Error('Discord client is unavailable.');
   if (client[STARTUP_KEY]) return client[STARTUP_KEY];
-
   const initialCheck = await socialScheduler.runSocialCheck(client, options);
   const schedulerTimer = socialScheduler.startSocialScheduler(client, options);
   const queueTimer = socialQueue.start(client, options.queue || {});
-  client[STARTUP_KEY] = {
-    startedAt: new Date().toISOString(),
-    initialCheck,
-    schedulerTimer,
-    queueTimer,
-  };
+  client[STARTUP_KEY] = { startedAt: new Date().toISOString(), initialCheck, schedulerTimer, queueTimer };
   return client[STARTUP_KEY];
 }
 
@@ -36,6 +38,7 @@ module.exports = {
   creators: socialCreators,
   simulator: socialSimulator,
   diagnostics: socialDiagnostics,
+  delivery: socialDelivery,
   providers: providerRegistry,
   scheduler: socialScheduler,
   startup,
