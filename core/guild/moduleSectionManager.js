a3aa642c4c608e3625e60e7ad8b479466b087e03
KSJ Hub <sycomorgan@gmail.com>
2026-07-20 08:44:21 +0100

    Remove unused module section manager bridge

--- core/guild/moduleSectionManager.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-// Temporary compatibility bridge for modules moved from src/modules/roleStudio/*
-// to src/modules/*. Remove after their relative imports are normalised.
-module.exports = require('../../src/core/guild/moduleSectionManager');
