4a1bdff2e03f45e92a23dd06aeeb3dc8b8ebc10e
KSJ Hub <sycomorgan@gmail.com>
2026-07-22 15:42:38 +0100

    Lazy-load canonical Social health API

--- src/modules/social/social.js
+++ src/modules/social/social.js
@@ -2,10 +2,16 @@
 
 const socialStore = require('./socialStore');
 const socialRuntime = require('./socialRuntime');
-const socialHealth = require('./socialHealth');
 
 module.exports = {
   ...socialRuntime,
   store: socialStore,
-  health: socialHealth,
 };
+
+Object.defineProperty(module.exports, 'health', {
+  enumerable: true,
+  configurable: false,
+  get() {
+    return require('./socialHealth');
+  },
+});
