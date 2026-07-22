484335001dc83cc5548fc94f55f1a774da92364b
KSJ Hub <sycomorgan@gmail.com>
2026-07-22 12:05:17 +0100

    Expose Social health through canonical entry

--- src/modules/social/social.js
+++ src/modules/social/social.js
@@ -2,8 +2,10 @@
 
 const socialStore = require('./socialStore');
 const socialRuntime = require('./socialRuntime');
+const socialHealth = require('./socialHealth');
 
 module.exports = {
   ...socialRuntime,
   store: socialStore,
+  health: socialHealth,
 };
