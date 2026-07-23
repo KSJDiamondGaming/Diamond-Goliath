2f4138b425ec13c93f1499bce66619d3bbed736e
KSJ Hub <sycomorgan@gmail.com>
2026-07-23 18:29:27 +0100

    feat(social): start provider incident monitoring

--- src/modules/social/social.js
+++ src/modules/social/social.js
@@ -2,12 +2,23 @@
 
 const socialStore = require('./socialStore');
 const socialRuntime = require('./socialRuntime');
+const incidentMonitor = require('./socialIncidentMonitor');
+
+async function startup(client, options = {}) {
+  const runtime = await socialRuntime.startup(client, options);
+  const incidentTimer = incidentMonitor.start(client, options.incidents || {});
+  return { ...runtime, incidentTimer };
+}
 
 module.exports = {
   ...socialRuntime,
+  startup,
   store: socialStore,
   http: require('./socialHttp'),
   providerHealth: require('./socialProviderHealth'),
+  providerIncidents: require('./socialProviderIncidents'),
+  incidentReporter: require('./socialIncidentReporter'),
+  incidentMonitor,
   pollingPolicy: require('./socialPollingPolicy'),
 };
 
