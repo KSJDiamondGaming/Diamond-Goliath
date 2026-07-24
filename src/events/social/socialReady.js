dd239938d00a80e896e5fc70360cd351a789f819
KSJ Hub <sycomorgan@gmail.com>
2026-07-24 10:59:32 +0100

    feat(social): register canonical shutdown lifecycle

--- src/events/social/socialReady.js
+++ src/events/social/socialReady.js
@@ -2,11 +2,13 @@
 
 const { Events } = require('discord.js');
 const social = require('../../modules/social/social');
+const socialProcessLifecycle = require('../../modules/social/socialProcessLifecycle');
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
     await social.startup(client);
+    socialProcessLifecycle.register(client);
   },
 };
