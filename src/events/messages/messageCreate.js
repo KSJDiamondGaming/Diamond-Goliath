54c3f5060e1ecaa0e124fce0aa06822500ae8e44
KSJ Hub <sycomorgan@gmail.com>
2026-07-26 04:56:01 +0100

    refactor: route leveling message tracking

--- src/events/messages/messageCreate.js
+++ src/events/messages/messageCreate.js
@@ -5,7 +5,7 @@ const { handleStickyMessage } = require('../../modules/messageStudio/sticky/stic
 const { handlePrefixCommand } = require('../../features/prefix/prefixRouter');
 const translationThreadManager = require('../../modules/utilityStudio/translation/translationThreadManager');
 const statsManager = require('../../modules/utilityStudio/stats/statsManager');
-const levelingManager = require('../../modules/communityStudio/leveling/levelingManager');
+const levelingTracking = require('../../modules/communityStudio/leveling/levelingTracking');
 
 module.exports = {
   name: Events.MessageCreate,
@@ -16,7 +16,7 @@ module.exports = {
       if (!message.content || message.author?.bot) return;
 
       await statsManager.handleMessageCreate(message);
-      await levelingManager.handleMessageCreate(message);
+      await levelingTracking.handleMessageCreate(message);
 
       const handledPrefixCommand = await handlePrefixCommand(message, client);
       if (handledPrefixCommand) return;
