2d172390ad3c24c0bd0bb57611745c4c9ff78dbd
KSJ Hub <sycomorgan@gmail.com>
2026-07-31 03:38:38 +0100

    refactor(mod): migrate dashboard permissions import

--- src/core/panels/mod/dashboardService.js
+++ src/core/panels/mod/dashboardService.js
@@ -35,7 +35,7 @@ const { normalizeDashboardContext } = require('./pendingActionHelpers');
 const {
   canUseModAction,
   getStaffDisplay,
-} = require('./moderationChecks');
+} = require('./permissions');
 
 const {
   COLORS,
