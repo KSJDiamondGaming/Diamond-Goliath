76f5187fe16a4cfc6ffa4826730c5b406824feae
KSJ Hub <sycomorgan@gmail.com>
2026-08-01 06:27:35 +0100

    refactor(giveaways): use canonical store gate for reaction adds

--- src/events/messages/messageReactionAdd.js
+++ src/events/messages/messageReactionAdd.js
@@ -2,6 +2,7 @@
 
 const { handleReactionAdd } = require('../../modules/roleStudio/reactionRoles/reactionRoles');
 const { enterGiveawayReaction } = require('../../modules/communityStudio/giveaways/giveawaysManager');
+const giveawaysStore = require('../../modules/communityStudio/giveaways/giveawaysStore');
 const { handleStarReactionAdd } = require('../../modules/messageStudio/starboard/starboard');
 const starboardStore = require('../../modules/messageStudio/starboard/starboardStore');
 const { isModuleEnabled } = require('../../core/guild/guildManager');
@@ -33,7 +34,7 @@ module.exports = {
     if (isModuleEnabled(guildId, 'reactionRoles')) {
       await runHandler('Reaction Roles', () => handleReactionAdd(reaction, user, client));
     }
-    if (isModuleEnabled(guildId, 'giveaways')) {
+    if (giveawaysStore.isEnabled(guildId)) {
       await runHandler('Giveaways', () => enterGiveawayReaction(reaction, user));
     }
     if (starboardStore.isEnabled(guildId)) {
