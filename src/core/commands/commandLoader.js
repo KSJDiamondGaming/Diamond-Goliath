7f21dda08a82534d4cf1a2373899a09eb8d183e9
KSJ Hub <sycomorgan@gmail.com>
2026-08-11 05:23:20 +0100

    Remove panel command loader exceptions

--- src/core/commands/commandLoader.js
+++ src/core/commands/commandLoader.js
@@ -24,13 +24,9 @@ function getAllJsFiles(dir) {
 
 function loadCommands(client, options = {}) {
   const commandsPath = options.commandsPath || path.join(process.cwd(), 'src', 'commands');
-  const userPanelCommand = path.join(process.cwd(), 'src', 'core', 'panels', 'user', 'user.js');
-  const modPanelCommand = path.join(process.cwd(), 'src', 'core', 'panels', 'mod', 'mod.js');
   const commandCenterCommand = path.join(process.cwd(), 'src', 'owner', 'auditIntelligence', 'auditEvents.js');
   const files = [
     ...getAllJsFiles(commandsPath),
-    ...(fs.existsSync(userPanelCommand) ? [userPanelCommand] : []),
-    ...(fs.existsSync(modPanelCommand) ? [modPanelCommand] : []),
     ...(fs.existsSync(commandCenterCommand) ? [commandCenterCommand] : []),
   ];
   const loaded = [];
