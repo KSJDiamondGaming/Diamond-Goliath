c57e045a4432e57940b18bc4b1f52f133526e37e
KSJ Hub <sycomorgan@gmail.com>
2026-08-12 12:42:09 +0100

    Use explicit Node built-ins in dev override manager

--- src/owner/dev/DevOverrideManager.js
+++ src/owner/dev/DevOverrideManager.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const { normalizeBotMode } = require('../../config/botModes');
 const { getRuntimePaths } = require('../../config/runtimePaths');
