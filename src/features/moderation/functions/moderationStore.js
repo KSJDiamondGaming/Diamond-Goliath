ff127b4955953f02162bf834637cc0dc8e8c9a9c
KSJ Hub <sycomorgan@gmail.com>
2026-08-12 11:44:39 +0100

    Use explicit Node built-ins in moderation store

--- src/features/moderation/functions/moderationStore.js
+++ src/features/moderation/functions/moderationStore.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 const { resolveRuntimePath } = require('../../../config/runtimePaths');
 const Database = require('better-sqlite3');
 
