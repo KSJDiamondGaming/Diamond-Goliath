2a1ab7373b5e1333f851e4f5f98ed65e5b7e659c
KSJ Hub <sycomorgan@gmail.com>
2026-08-13 05:44:29 +0100

    Use explicit Node crypto import in notification store

--- src/core/notifications/notificationStore.js
+++ src/core/notifications/notificationStore.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const crypto = require('crypto');
+const crypto = require('node:crypto');
 const { getGuildSection, updateGuildSection } = require('../guild/guildManager');
 const activity = require('../../features/activity/activityStore');
 
