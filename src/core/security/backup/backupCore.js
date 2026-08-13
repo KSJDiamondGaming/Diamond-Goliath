04b7aa8eae533e8caf9f0efee5ffe0f8915e3728
KSJ Hub <sycomorgan@gmail.com>
2026-08-13 04:51:06 +0100

    Use explicit Node built-ins in backup core

--- src/core/security/backup/backupCore.js
+++ src/core/security/backup/backupCore.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const crypto = require('crypto');
-const fs = require('fs');
-const path = require('path');
+const crypto = require('node:crypto');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const { resolveBotMode, resolveRuntimePath } = require('../../../config/runtimePaths');
 
