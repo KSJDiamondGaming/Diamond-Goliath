da240bb08bd10477908b8590eb4c329ad71afbfd
KSJ Hub <sycomorgan@gmail.com>
2026-08-15 12:22:41 +0100

    Use explicit Node built-ins in media service

--- src/core/mediaTools/mediaService.js
+++ src/core/mediaTools/mediaService.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const { DISCORD_LIMITS, UPLOAD_LIMITS } = require('./mediaConfig');
 const {
