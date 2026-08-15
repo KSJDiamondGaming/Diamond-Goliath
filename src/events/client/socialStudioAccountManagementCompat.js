b441d9e9f1c40af31a63244ffeac3719f0ccea06
KSJ Hub <sycomorgan@gmail.com>
2026-08-15 19:59:04 +0100

    refactor(social): remove account management self patch

--- src/events/client/socialStudioAccountManagementCompat.js
+++ src/events/client/socialStudioAccountManagementCompat.js
@@ -11,7 +11,6 @@ const {
   TextInputStyle,
 } = require('discord.js');
 const crypto = require('node:crypto');
-const creatorCompat = require('../../modules/socialStudio/socialAlerts/socialStudioCreatorActionCompat');
 const store = require('../../modules/socialStudio/socialAlerts/socialStudioStore');
 const socialPanel = require('../../modules/socialStudio/socialAlerts/socialStudioPanel');
 const { normalizeAccountInput } = require('../../modules/socialStudio/socialAlerts/accountNormalizer');
@@ -394,20 +393,11 @@ async function handle(interaction) {
   return false;
 }
 
-if (!creatorCompat.__accountManagementCompatPatched) {
-  const originalHandle = typeof creatorCompat.handle === 'function' ? creatorCompat.handle.bind(creatorCompat) : async () => false;
-  creatorCompat.handle = async function handleWithAccountManagement(interaction) {
-    if (await handle(interaction)) return true;
-    return originalHandle(interaction);
-  };
-  creatorCompat.__accountManagementCompatPatched = true;
-}
-
 module.exports = {
   name: 'clientReady',
   once: true,
   handle,
   async execute() {
-    // Loading this file installs the account-management compatibility wrapper.
+    // Account management is routed explicitly by Social Studio creator routing.
   },
 };
