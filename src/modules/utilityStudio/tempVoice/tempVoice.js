aba443874fc6f61845ef9d2b0294a0989f920e50
KSJ Hub <sycomorgan@gmail.com>
2026-08-15 18:38:21 +0100

    Remove unused temp voice startup facade

--- src/modules/utilityStudio/tempVoice/tempVoice.js
+++ src/modules/utilityStudio/tempVoice/tempVoice.js
@@ -4,15 +4,12 @@ const tempVoiceRuntime = require('./tempVoiceManager');
 const tempVoiceStore = require('./tempVoiceStore');
 const tempVoicePanel = require('./tempVoicePanel');
 const tempVoiceHealth = require('./tempVoiceHealth');
-const tempVoiceStartup = require('./tempVoiceStartup');
 
 module.exports = {
   ...tempVoiceRuntime,
   store: tempVoiceStore,
   panel: tempVoicePanel,
   health: tempVoiceHealth,
-  startup: tempVoiceStartup.startup,
-  shutdown: tempVoiceStartup.shutdown,
   getConfig: tempVoiceStore.getTempVoiceSection,
   getHubs: tempVoiceStore.getHubs,
   getHub: tempVoiceStore.getHub,
