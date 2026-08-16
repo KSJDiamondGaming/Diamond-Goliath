e74ea01c9b534ca97efbc184158732c1673b0e7b
KSJ Hub <sycomorgan@gmail.com>
2026-08-16 03:00:53 +0100

    Always install scheduled welcome scheduler

--- src/events/client/welcomeStartup.js
+++ src/events/client/welcomeStartup.js
@@ -8,6 +8,8 @@ const scheduledWelcomeScheduler = require('../../modules/messageStudio/welcome/s
 module.exports = {
   name: Events.ClientReady,
   async execute(client) {
+    await scheduledWelcomeScheduler.startup(client);
+
     const enabledGuilds = client.guilds.cache.filter((guild) => guildManager.isModuleEnabled(guild.id, 'welcome'));
     if (!enabledGuilds.size) {
       console.log('[Welcome] Startup check skipped: no enabled guilds.');
@@ -18,7 +20,5 @@ module.exports = {
       ...client,
       guilds: { ...client.guilds, cache: enabledGuilds },
     });
-
-    await scheduledWelcomeScheduler.startup(client);
   },
 };
