f4ed1ff383a4ef11ea15b7967eed480f96837043
KSJ Hub <sycomorgan@gmail.com>
2026-08-17 22:44:02 +0100

    refactor(embed): source button limit from canonical panel

--- src/modules/messageStudio/embed/embedButtonsCompat.js
+++ src/modules/messageStudio/embed/embedButtonsCompat.js
@@ -17,7 +17,7 @@ const guildManager = require('../../../core/guild/guildManager');
 const { getAllEmbedDeployments } = require('./embedDeployments');
 const panel = require('./embedPanel');
 
-const MAX_BUTTONS = 20;
+const MAX_BUTTONS = panel.MAX_BUTTONS || 20;
 const MAX_COMPONENTS_PER_ROW = panel.EMBED_COMPONENT_LIMITS?.maxComponentsPerRow || 5;
 const MAX_ACTION_ROWS = panel.EMBED_COMPONENT_LIMITS?.maxActionRows || 5;
 const MAX_DEPLOYED_BUTTON_ROWS = 4;
