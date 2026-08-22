293cdb2965523bbe609d2350425e88eb61dfbfd9
KSJ Hub <sycomorgan@gmail.com>
2026-08-22 12:47:33 +0100

    Install Timed Roles compatibility hardening at startup

--- src/events/client/timedRolesStartup.js
+++ src/events/client/timedRolesStartup.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { Events } = require('discord.js');
+require('../../modules/roleStudio/timedRoles/timedRolesCompat');
 const timedRoles = require('../../modules/roleStudio/timedRoles/timedRolesService');
 
 module.exports = {
