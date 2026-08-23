df39a07a324e8ec4546de5aede5ee79df531fc1e
KSJ Hub <sycomorgan@gmail.com>
2026-08-23 06:29:01 +0100

    Route moderation through core systems domain

---
+++ src/core/systems/mod/cases.js
@@ -0,0 +1,2 @@
+'use strict';
+module.exports = require('../../panels/mod/cases');
--- src/core/systems/mod/panel.js
+++ src/core/systems/mod/panel.js
@@ -1,4 +1,2 @@
 'use strict';
-
-// Transitional entry point while the Mod domain is consolidated into core/systems/mod.
 module.exports = require('../../panels/mod/modPanel');
