76209ef1ce2b0c34a7c3d2c027b676f450c345da
KSJ Hub <sycomorgan@gmail.com>
2026-08-24 04:16:29 +0100

    Point User panel entry to flat implementation

--- src/core/systems/user/panel.js
+++ src/core/systems/user/panel.js
@@ -1,4 +1,2 @@
 'use strict';
-
-// Transitional entry point while the User domain is consolidated into core/systems/user.
-module.exports = require('../../user/panel');
+module.exports = require('./userPanel');
