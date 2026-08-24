dcc87bab2edd6eb39cc289341f7878956856a44c
KSJ Hub <sycomorgan@gmail.com>
2026-08-24 04:36:25 +0100

    Remove legacy Mod case storage bridge

--- src/core/logging/cases/caseStore.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-// Compatibility entry while legacy imports are removed.
-// The real moderation case storage now lives in core/systems/mod/storage.js.
-module.exports = require('../../systems/mod/storage');
