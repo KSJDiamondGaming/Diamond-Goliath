31c22c935fc4427c501fd3c9e92e85b3714ca124
KSJ Hub <sycomorgan@gmail.com>
2026-08-25 02:14:06 +0100

    Remove legacy moderation log shim

--- src/core/logging/modlogs/moderationActionLog.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const storage = require('../../systems/mod/storage');
-
-module.exports = storage.logModerationAction;
-module.exports.logModerationAction = storage.logModerationAction;
-module.exports.sendModLog = storage.sendModLog;
