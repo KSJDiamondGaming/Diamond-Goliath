6e28a8e75641e625bed98d9662c5f9cbdad081e6
KSJ Hub <sycomorgan@gmail.com>
2026-08-25 03:15:34 +0100

    Point Auto Roles panel directly at protection security

--- src/modules/roleStudio/autoRoles/autoRolesPanel.js
+++ src/modules/roleStudio/autoRoles/autoRolesPanel.js
@@ -8,8 +8,8 @@ const {
   RoleSelectMenuBuilder,
   AttachmentBuilder,
 } = require('discord.js');
-const security = require('../../../core/security/securityCore');
-const { validateRoleSelection } = require('../../../core/security/goliathPermissionGuard');
+const security = require('../../../core/systems/security/protection/core');
+const { validateRoleSelection } = require('../../../core/systems/security/protection/permissions');
 const autoRoles = require('./autoRolesService');
 
 function row(...components) {
