7b43c87e235f376adfdd1a889fb995336dbf7975
KSJ Hub <sycomorgan@gmail.com>
2026-08-26 16:07:10 +0100

    refactor(security): route channel security through protection subsystem

--- src/events/channels/channelDelete.js
+++ src/events/channels/channelDelete.js
@@ -1,8 +1,8 @@
 const { AuditLogEvent } = require('discord.js');
 
 const guildManager = require('../../core/guild/guildManager');
-const securitySystem = require('../../core/security/securitySystem');
-const antiNukeManager = require('../../core/security/antiNukeManager');
+const securitySystem = require('../../core/systems/security/protection/system');
+const antiNukeManager = require('../../core/systems/security/protection/antiNuke');
 const {
   emitSyncEvent,
 } = require('../../server/sockets/socketHub');
