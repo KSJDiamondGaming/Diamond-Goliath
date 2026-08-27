445c484f95b26651bf283c68673ab01741424263
KSJ Hub <sycomorgan@gmail.com>
2026-08-27 22:04:39 +0100

    keep translate shim doctor compatible

--- src/commands/utility/translate.js
+++ src/commands/utility/translate.js
@@ -1,3 +1,8 @@
 'use strict';
 
-module.exports = require('../../core/systems/user/utilities').adapters.translate;
+const adapter = require('../../core/systems/user/utilities').adapters.translate;
+module.exports = {
+  hidden: true,
+  data: { name: 'translate', description: 'User panel translation utility', toJSON: () => ({ name: 'translate', description: 'User panel translation utility' }) },
+  execute: adapter.execute,
+};
