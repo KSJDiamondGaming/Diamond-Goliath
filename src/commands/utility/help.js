b895ed13c0234310ef2a606705846811ad443c91
KSJ Hub <sycomorgan@gmail.com>
2026-08-27 22:04:25 +0100

    keep help shim doctor compatible

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,3 +1,8 @@
 'use strict';
 
-module.exports = require('../../core/systems/user/utilities').adapters.help;
+const adapter = require('../../core/systems/user/utilities').adapters.help;
+module.exports = {
+  hidden: true,
+  data: { name: 'help', description: 'User panel help utility', toJSON: () => ({ name: 'help', description: 'User panel help utility' }) },
+  execute: adapter.execute,
+};
