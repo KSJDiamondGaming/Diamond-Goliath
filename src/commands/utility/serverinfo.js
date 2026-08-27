cc89ce7511e1e3f829c313aa3affe1749e45cdd2
KSJ Hub <sycomorgan@gmail.com>
2026-08-27 22:04:31 +0100

    keep server info shim doctor compatible

--- src/commands/utility/serverinfo.js
+++ src/commands/utility/serverinfo.js
@@ -1,3 +1,8 @@
 'use strict';
 
-module.exports = require('../../core/systems/user/utilities').adapters.serverinfo;
+const adapter = require('../../core/systems/user/utilities').adapters.serverinfo;
+module.exports = {
+  hidden: true,
+  data: { name: 'serverinfo', description: 'User panel server information utility', toJSON: () => ({ name: 'serverinfo', description: 'User panel server information utility' }) },
+  execute: adapter.execute,
+};
