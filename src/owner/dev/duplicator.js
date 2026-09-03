e76ec2a55f03e0ba417174753205a19dd2d4b482
KSJ Hub <sycomorgan@gmail.com>
2026-09-03 02:11:27 +0100

    Start duplicator bridge for every Goliath environment

--- src/owner/dev/duplicator.js
+++ src/owner/dev/duplicator.js
@@ -1,3 +1,17 @@
 'use strict';
 
-module.exports = require('./duplicatorV2');
+const { Client } = require('discord.js');
+const duplicator = require('./duplicatorV2');
+
+const BOOTSTRAP_KEY = Symbol.for('goliath.duplicator.bridge-bootstrap');
+
+if (!Client.prototype[BOOTSTRAP_KEY]) {
+  const originalLogin = Client.prototype.login;
+  Object.defineProperty(Client.prototype, BOOTSTRAP_KEY, { value: true });
+  Client.prototype.login = function goliathDuplicatorLogin(...args) {
+    duplicator.initializeBridge(this);
+    return originalLogin.apply(this, args);
+  };
+}
+
+module.exports = duplicator;
