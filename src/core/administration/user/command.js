2abc5a16fe211917a7171b20ca36ed79aa279593
KSJ Hub <sycomorgan@gmail.com>
2026-09-05 00:42:33 +0100

    Keep /user focused on the user panel

--- src/core/administration/user/command.js
+++ src/core/administration/user/command.js
@@ -3,7 +3,6 @@ const { SlashCommandBuilder } = require('discord.js');
 const { errorEmbed } = require('../../ui/embeds');
 const { enforceCommandAccess } = require('../../commands/commandAccess');
 const { buildUserHomePanel } = require('./interactions');
-const emojisUserPanel = require('../../../modules/utilityStudio/emojis/emojisUserPanel');
 
 module.exports = {
   category: 'Utility',
@@ -21,20 +20,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('user')
     .setDescription('Open your Goliath user panel')
-    .addStringOption((option) => option
-      .setName('emoji')
-      .setDescription('Quickly find an emoji from Goliath Core or this server')
-      .setAutocomplete(true)
-      .setRequired(false))
-    .addBooleanOption((option) => option
-      .setName('emoji_browser')
-      .setDescription('Open your searchable Emoji Studio browser')
-      .setRequired(false))
     .setDMPermission(false),
-
-  async autocomplete(interaction) {
-    return emojisUserPanel.autocomplete(interaction);
-  },
 
   async execute(interaction) {
     const denied = await enforceCommandAccess(interaction, module.exports);
@@ -45,22 +31,6 @@ module.exports = {
         return await safeReply(interaction, {
           embeds: [errorEmbed('This command can only be used inside a server.')],
         });
-      }
-
-      if (interaction.options.getBoolean('emoji_browser') === true) {
-        return await safeReply(interaction, await emojisUserPanel.buildPanel(interaction));
-      }
-
-      const emojiId = interaction.options.getString('emoji');
-      if (emojiId) {
-        const selection = await emojisUserPanel.commandSelection(interaction, emojiId);
-        if (!selection) {
-          return await safeReply(interaction, {
-            embeds: [errorEmbed('That emoji is no longer available in this server.')],
-            components: [],
-          });
-        }
-        return await safeReply(interaction, selection);
       }
 
       return await safeReply(interaction, buildUserHomePanel(interaction));
